ad1abd2f58868bef3262b6f24637adf65a1fe842
Muhammad <89138415+Grom013@users.noreply.github.com>
2024-01-05 01:27:00 +0300

    fix postSize

--- public/assets/post-size.js
+++ public/assets/post-size.js
@@ -1,13 +1,12 @@
 export default function postSize(str) {
-  let end = ['.com', '.org', '.net', '.ru', '.io', '.gov', '.edu', '.uk', '.de', '.jp']
-  let start = ['http://', 'https://', 'www.']
-  str= str.split(' ')
-  let arr=[]
-  str.forEach(w=>{
-    if(end.some(e=>w.includes(e))||start.some(s=>w.includes(s))){
-      arr.push(w)
+  const end = ['.com', '.org', '.net', '.ru', '.io', '.gov', '.edu', '.uk', '.de', '.jp'];
+  const start = ['http://', 'https://', 'www.'];
+  const arr = [];
+  str.split(' ').forEach((w) => {
+    if (end.some((e) => w.includes(e)) || start.some((s) => w.includes(s))) {
+      arr.push(w);
     }
-  })
-  str = str.filter(w=>!arr.includes(w))
-  return str.join(' ').length
+  });
+  const words = str.split(' ').filter((w) => !arr.includes(w));
+  return words.join(' ').length;
 }
