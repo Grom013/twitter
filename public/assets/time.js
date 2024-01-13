cfa338db4413f14f9c7a9bc21341e7afcff10f03
Muhammad <89138415+Grom013@users.noreply.github.com>
2024-01-14 02:59:47 +0300

    fix eslint number-time

--- public/assets/time.js
+++ public/assets/time.js
@@ -1,55 +1,52 @@
 export default function time(min) {
-    function conjugationMinutes(min) {
-        const remainderOfTenMinutes = min % 10;
-        const remainderOfHundredMinutes = min % 100;
-        if (remainderOfHundredMinutes >= 11 && remainderOfHundredMinutes <= 20) {
-            return `${min} минут назад`;
-        }
-        if (remainderOfTenMinutes === 1) {
-            return `${min} минуту назад`;
-        } else if (remainderOfTenMinutes >= 2 && remainderOfTenMinutes <= 4) {
-            return `${min} минуты назад`;
-        } else {
-            return `${min} минут назад`;
-        }
+  function conjugationMinutes(min) {
+    const remainderOfTenMinutes = min % 10;
+    const remainderOfHundredMinutes = min % 100;
+    if (remainderOfHundredMinutes >= 11 && remainderOfHundredMinutes <= 20) {
+      return `${min} минут назад`;
     }
-
-    const hour = Math.floor(min / 60);
-
-    function conjugationHours(hour) {
-        const remainderOfTenHours = hour % 10;
-        if (hour === 1 || remainderOfTenHours === 1) {
-            return `${hour} час назад`;
-        } else if ((hour >= 2 && hour <= 4) || (remainderOfTenHours <= 4 && remainderOfTenHours >= 2)) {
-            return `${hour} часа назад`;
-        } else if (hour >= 5 && hour <= 20) {
-            return `${hour} часов назад`;
-        }
+    if (remainderOfTenMinutes === 1) {
+      return `${min} минуту назад`;
+    } if (remainderOfTenMinutes >= 2 && remainderOfTenMinutes <= 4) {
+      return `${min} минуты назад`;
     }
+    return `${min} минут назад`;
+  }
 
-    const day = Math.floor(hour / 24);
+  const hour = Math.floor(min / 60);
 
-    function conjugationDays(day) {
-        const remainderOfTenDays = day % 10;
-        const remainderOfHundredDays = day % 100;
-        if (remainderOfHundredDays >= 11 && remainderOfHundredDays <= 20) {
-            return `${day} дней назад`;
-        } else if (remainderOfTenDays === 1) {
-            return `${day} день назад`;
-        } else if (remainderOfTenDays >= 2 && remainderOfTenDays <= 4) {
-            return `${day} дня назад`;
-        } else {
-            return `${day} дней назад`;
-        }
+  function conjugationHours(hour) {
+    const remainderOfTenHours = hour % 10;
+    if (hour === 1 || remainderOfTenHours === 1) {
+      return `${hour} час назад`;
+    } if ((hour >= 2 && hour <= 4) || (remainderOfTenHours <= 4 && remainderOfTenHours >= 2)) {
+      return `${hour} часа назад`;
+    } if (hour >= 5 && hour <= 20) {
+      return `${hour} часов назад`;
     }
+  }
 
-    if (min < 60) {
-        return conjugationMinutes(min);
-    } else if (min >= 60 && min < 1440) {
-        return conjugationHours(hour);
-    } else if (day >= 1 && day <= 365) {
-        return conjugationDays(day);
-    } else {
-        return `более года назад`;
+  const day = Math.floor(hour / 24);
+
+  function conjugationDays(day) {
+    const remainderOfTenDays = day % 10;
+    const remainderOfHundredDays = day % 100;
+    if (remainderOfHundredDays >= 11 && remainderOfHundredDays <= 20) {
+      return `${day} дней назад`;
+    } if (remainderOfTenDays === 1) {
+      return `${day} день назад`;
+    } if (remainderOfTenDays >= 2 && remainderOfTenDays <= 4) {
+      return `${day} дня назад`;
     }
+    return `${day} дней назад`;
+  }
+
+  if (min < 60) {
+    return conjugationMinutes(min);
+  } if (min >= 60 && min < 1440) {
+    return conjugationHours(hour);
+  } if (day >= 1 && day <= 365) {
+    return conjugationDays(day);
+  }
+  return 'более года назад';
 }
--- tests/time.test.js
+++ tests/time.test.js
@@ -1,60 +1,60 @@
 import { assert } from 'chai';
-import time from "../public/assets/time.js";
+import time from '../public/assets/time.js';
 
 describe('Функция преобразования числа во время', function () {
-    it('выходим за рамки одного года', function () {
-        const expectedResult = 'более года назад';
-        const result = time(366*24*60);
-        assert.equal(expectedResult, result);
-    });
-    it('выходим за рамки рамки 60 минут', function () {
-        const expectedResult = '1 час назад';
-        const result = time(63);
-        assert.equal(expectedResult, result);
-    });
-    it('выходим за рамки рамки 24 часов', function () {
-        const expectedResult = '1 день назад';
-        const result = time(25*60);
-        assert.equal(expectedResult, result);
-    });
-    it('проверяем спряжение', function () {
-        const expectedResult = '2 минуты назад';
-        const result = time(2);
-        assert.equal(expectedResult, result);
-    });
-    it('проверяем спряжение', function () {
-        const expectedResult = '5 минут назад';
-        const result = time(5);
-        assert.equal(expectedResult, result);
-    });
-    it('проверяем спряжение', function () {
-        const expectedResult = '21 минуту назад';
-        const result = time(21);
-        assert.equal(expectedResult, result);
-    });
-    it('проверяем спряжение', function () {
-        const expectedResult = '11 минут назад';
-        const result = time(11);
-        assert.equal(expectedResult, result);
-    });
-    it('проверяем спряжение', function () {
-        const expectedResult = '2 часа назад';
-        const result = time(2*60);
-        assert.equal(expectedResult, result);
-    });
-    it('проверяем спряжение', function () {
-        const expectedResult = '6 часов назад';
-        const result = time(6*60);
-        assert.equal(expectedResult, result);
-    });
-    it('проверяем спряжение', function () {
-        const expectedResult = '2 дня назад';
-        const result = time(2*24*60);
-        assert.equal(expectedResult, result);
-    });
-    it('проверяем спряжение', function () {
-        const expectedResult = '2 дня назад';
-        const result = time(2*24*60);
-        assert.equal(expectedResult, result);
-    });
+  it('выходим за рамки одного года', function () {
+    const expectedResult = 'более года назад';
+    const result = time(366 * 24 * 60);
+    assert.equal(expectedResult, result);
+  });
+  it('выходим за рамки рамки 60 минут', function () {
+    const expectedResult = '1 час назад';
+    const result = time(63);
+    assert.equal(expectedResult, result);
+  });
+  it('выходим за рамки рамки 24 часов', function () {
+    const expectedResult = '1 день назад';
+    const result = time(25 * 60);
+    assert.equal(expectedResult, result);
+  });
+  it('проверяем спряжение', function () {
+    const expectedResult = '2 минуты назад';
+    const result = time(2);
+    assert.equal(expectedResult, result);
+  });
+  it('проверяем спряжение', function () {
+    const expectedResult = '5 минут назад';
+    const result = time(5);
+    assert.equal(expectedResult, result);
+  });
+  it('проверяем спряжение', function () {
+    const expectedResult = '21 минуту назад';
+    const result = time(21);
+    assert.equal(expectedResult, result);
+  });
+  it('проверяем спряжение', function () {
+    const expectedResult = '11 минут назад';
+    const result = time(11);
+    assert.equal(expectedResult, result);
+  });
+  it('проверяем спряжение', function () {
+    const expectedResult = '2 часа назад';
+    const result = time(2 * 60);
+    assert.equal(expectedResult, result);
+  });
+  it('проверяем спряжение', function () {
+    const expectedResult = '6 часов назад';
+    const result = time(6 * 60);
+    assert.equal(expectedResult, result);
+  });
+  it('проверяем спряжение', function () {
+    const expectedResult = '2 дня назад';
+    const result = time(2 * 24 * 60);
+    assert.equal(expectedResult, result);
+  });
+  it('проверяем спряжение', function () {
+    const expectedResult = '2 дня назад';
+    const result = time(2 * 24 * 60);
+    assert.equal(expectedResult, result);
+  });
 });
