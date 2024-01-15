d3485de0e30349d8b490ff8b2aaf0cff53ef5285
Muhammad <89138415+Grom013@users.noreply.github.com>
2024-01-16 00:34:16 +0300

    add hashtag.js

---
+++ public/assets/hashtag.js
@@ -0,0 +1,10 @@
+function hashTag(str){
+    return str.split(' ').map((s)=>{
+        if(s.startsWith('#')){
+            return  s.replace(s,`<a href="/search?tag=${s.slice(1)}" >${s}</a>`)
+        }
+        return s
+    }).join(' ')
+}
+
+console.log(hashTag("Кто еще изучает #javascript ?"))
