3dfe6e6a95fbd57b651800c1fda348f0bcf2eb93
Muhammad <89138415+Grom013@users.noreply.github.com>
2024-01-18 22:06:43 +0300

    fix eslint for hashtag.test.js

--- tests/hashrag.test.js
+++ tests/hashrag.test.js
@@ -1,10 +1,10 @@
 import { assert } from 'chai';
-import hashTag from "../public/assets/hashtag.js";
+import hashTag from '../public/assets/hashtag.js';
 
 describe('Функция перевода хештега в ссылку', function () {
-    it('правильная ссылка', function () {
-        const expectedResult = 'Кто еще изучает <a href="/search?tag=javascript" >#javascript</a> ?';
-        const result = hashTag('Кто еще изучает #javascript ?');
-        assert.equal(expectedResult, result);
-    });
+  it('правильная ссылка', function () {
+    const expectedResult = 'Кто еще изучает <a href="/search?tag=javascript" >#javascript</a> ?';
+    const result = hashTag('Кто еще изучает #javascript ?');
+    assert.equal(expectedResult, result);
+  });
 });
