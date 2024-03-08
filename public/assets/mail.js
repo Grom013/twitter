67eef4aed6a5fb3633e971e219b47e4afdaaac05
Grom Gromov <khalid.096@bk.ru>
2024-03-09 03:20:16 +0400

    add modals

--- public/assets/mail.js
+++ public/assets/mail.js
@@ -1,20 +1,29 @@
 export default function mail(str) {
+  if (typeof str !== 'string' || !str) {
+    return false;
+  }
+
   const start = ['.', ',', '-', '@', '=', ';', ':'];
   const invalidSymbol = ['[', ']', ';', ':'];
-  const [first, second] = str.split('@');
-  if (invalidSymbol.some((i) => str.includes(i))) {
-    return false;
-  }
-  if (start.some((s) => str.startsWith(s))) {
-    return false;
-  }
-  if (
-    first.length <= 3
+
+  if (str.includes('@')) {
+    const [first, second] = str.split('@');
+    if (invalidSymbol.some((i) => str.includes(i))) {
+      return false;
+    }
+    if (start.some((s) => str.startsWith(s))) {
+      return false;
+    }
+    if (
+      first.length <= 3
         || !second.includes('.')
         || second.indexOf('.') <= 1
         || second.length - second.indexOf('.') <= 2
-  ) {
-    return false;
+    ) {
+      return false;
+    }
+    return true;
   }
-  return true;
+
+  return false;
 }
