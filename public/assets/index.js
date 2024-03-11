90323541a17c9a284cb972ada18eee41834b0dbf
Grom Gromov <khalid.096@bk.ru>
2024-03-11 17:27:08 +0400

    add jsonMain

--- public/assets/index.js
+++ public/assets/index.js
@@ -1,4 +1,5 @@
 import mail from './mail.js';
+import { fetchData, fetchPictures } from './fetchData.js';
 
 const closeModal = document.querySelectorAll('.close');
 const openModal = document.querySelector('.btn-reg');
@@ -13,6 +14,68 @@ const emailInputDanger = document.querySelector('.email-input-danger');
 const passwordInputDanger = document.querySelector('.password-input-danger');
 const confirmPasswordInputDanger = document.querySelector('.confirm-password-input-danger');
 const emailAddressInputDanger = document.querySelector('.email-address-input-danger');
+const usersElement = document.getElementById('users');
+const todayMessagesElement = document.getElementById('todayMessages');
+const allMessagesElement = document.getElementById('allMessages');
+
+function displayMessages(messages, pictures) {
+  const messageContainer = document.querySelector('.last-messages');
+  if (!messageContainer) return;
+
+  let messagesHtml = '';
+  messages.forEach((message) => {
+    const picture = pictures.find((pic) => pic.id === message.id);
+    const pictureUrl = picture ? picture.avatarUrl : '';
+    let postsImg = '';
+    if (message.img) {
+      postsImg = `<img class="posts-img" src="${message.img}" alt="" />`;
+    }
+    messagesHtml += `
+      <div class="last-messages-post">
+        <a class="post-avatar"><img src="${pictureUrl}" alt=""/></a>
+        <div class="message-info">
+          <div class="message-info-first">
+            <div class="post-name">
+              <div class="post-main-name">${message.name} ${message.lastname}</div>
+              <div class="post-nickname">@${message.nickName}</div>
+            </div>
+            <div class="post-time">${message.timeMessage} минут назад</div>
+          </div>
+          <div class="message-desc">${message.message}</div>
+          
+          ${postsImg}
+          <div class="post-icons">
+            <div class="post-repost"><img alt="icon1" src="img/repost.svg"/> ${message.repost}</div>
+            <div class="post-like"><img alt="icon2" src="img/like.svg"/> ${message.like}</div>
+            <div class="post-share"><img alt="icon3" src="img/share.svg"/> ${message.share}</div>
+          </div>
+        </div>
+      </div>
+      <div class="border-post"></div>
+      <div class="whitening"></div>
+    `;
+  });
+
+  messageContainer.innerHTML = messagesHtml;
+}
+
+async function view() {
+  try {
+    const data = await fetchData();
+    const pictures = await fetchPictures();
+    usersElement.textContent = data.static.users;
+    allMessagesElement.textContent = data.static.allMessages;
+    todayMessagesElement.textContent = data.static.todayMessages;
+
+    if (data && pictures) {
+      displayMessages(data.lastMessages, pictures.pictures);
+    }
+  } catch (error) {
+    console.error('Error', error);
+  }
+}
+
+view();
 submitBtn.addEventListener('click', () => {
   const name = nameInput.value.trim();
   const email = emailInput.value.trim();
@@ -30,11 +93,6 @@ submitBtn.addEventListener('click', () => {
     nameInput.classList.add('red-field');
     nameInputDanger.style.display = 'block';
   }
-  // if (email === '') {
-  //   emailInput.classList.remove('input-field');
-  //   emailInput.classList.add('red-field');
-  //   emailInputDanger.style.display = 'block';
-  // }
   if (password === '') {
     passwordInput.classList.remove('input-field');
     passwordInput.classList.add('red-field');
