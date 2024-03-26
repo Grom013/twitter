b2fc0389ceb761d722a1e74f897c3343aa607540
Grom Gromov <khalid.096@bk.ru>
2024-03-26 06:21:34 +0400

    Remove node_modules from tracking and add new files

---
+++ src/Header/Header.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import './Header.css';
+
+function Header() {
+  return (
+      <div className="container">
+          <div className="header">
+              <img src="img/Vector.svg" alt="" />
+              <div className="header_logo" />
+              <div className="header_text">
+                  Оставайся на связи с друзьями, даже когда их нет рядом
+              </div>
+              <button className="btn-reg">Зарегистрироваться</button>
+              <button className="btn-login">Войти</button>
+          </div>
+          <div className="header-img" />
+      </div>
+  );
+}
+
+export default Header;
