e0353b022a3cdde2d671e06a1b50f861f9f3345a
Grom Gromov <khalid.096@bk.ru>
2024-03-30 01:10:06 +0400

    add aa

--- app.js
+++ app.js
@@ -11,5 +11,5 @@ app.use(express.static('public'));
 app.get('/', (req, res) => res.type('html').send(html));
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`Example app listening on pordt ${port}`);
 });
