9b54f67df94088bddcf12087a49c973240a3283e
Grom Gromov <khalid.096@bk.ru>
2024-03-30 01:12:36 +0400

    add aa

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
 import express from 'express';
 import fs from 'fs';
+import path from 'path';
 
 const app = express();
 const port = 3000;
 
-const html = fs.readFileSync('public/main.html', 'utf8');
+// Получаем абсолютный путь к файлу main.html
+const htmlPath = path.join(__dirname, 'public', 'main.html');
+const html = fs.readFileSync(htmlPath, 'utf8');
 
 app.use(express.static('public'));
 
 app.get('/', (req, res) => res.type('html').send(html));
 
 app.listen(port, () => {
-  console.log(`Example app listening on pordt ${port}`);
+  console.log(`Example app listening on port ${port}`);
 });
