708e4865f9027ccd0763a2de994f0ed367929d07
Grom Gromov <khalid.096@bk.ru>
2024-04-02 19:03:46 +0400

    asd

--- app.js
+++ app.js
@@ -1,18 +1,14 @@
 import express from 'express';
-import cors from 'cors';
-// import fs from 'fs';
+import fs from 'fs';
 
 const app = express();
 const port = 3000;
 
-// const html = fs.readFileSync('public/main.html', 'utf8');
+const html = fs.readFileSync('public/main.html', 'utf8');
 
 app.use(express.static('public'));
-app.use(cors());
 
-app.get('/posts', (req, res) => {
-  res.type('json').send({ posts: [{ id: 1, message: 'hello' }, { id: 2, message: 'hello2' }] });
-});
+app.get('/', (req, res) => res.type('html').send(html));
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
