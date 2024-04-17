a4db3128e506a862b535681d41b4d735abddbeef
Grom Gromov <khalid.096@bk.ru>
2024-04-17 19:27:28 +0300

    add SQL

--- public/assets/fetchData.js
+++ public/assets/fetchData.js
@@ -1,20 +1,34 @@
 export async function fetchData() {
   try {
-    const response = await fetch('/data.json');
-    if (!response.ok) {
-      throw new Error('error network');
+    const responseTopics = fetch('http://localhost:3000/topics.json');
+    const responseLastMessages = fetch('http://localhost:3000/lastMessages.json');
+    const responseBlogs = fetch('http://localhost:3000/blogs.json');
+
+    const [topicsResponse, lastMessagesResponse, blogsResponse] = await Promise.all([
+      responseTopics,
+      responseLastMessages,
+      responseBlogs,
+    ]);
+
+    if (!topicsResponse.ok || !lastMessagesResponse.ok || !blogsResponse.ok) {
+      throw new Error('Network error');
     }
-    const data = await response.json();
-    return data;
+    const topicsData = await topicsResponse.json();
+    const lastMessagesData = await lastMessagesResponse.json();
+    const blogsData = await blogsResponse.json();
+
+    return { topics: topicsData, lastMessages: lastMessagesData, blogs: blogsData };
   } catch (error) {
-    console.error('Error', error);
+    console.error('Error:', error);
     return null;
   }
 }
 
 export async function fetchPictures() {
   try {
-    const response = await fetch('/pictures.json');
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+
+    const response = await fetch('/public/pictures.json');
     if (!response.ok) {
       throw new Error('error network');
     }
