f5d9950d6a1754bea48c430772fd41d9e7173d6f
Grom Gromov <khalid.096@bk.ru>
2024-04-19 13:00:35 +0300

    aslmfvassdassdsdsdsdd

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ import pkg from 'pg';
 
 const { Pool } = pkg;
 const app = express();
-const port = 3000;
+const port = process.env.port ?? 3000;
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
