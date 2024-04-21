5c42a2f83a734732f16b2db3d65807d77f0813e1
Grom Gromov <khalid.096@bk.ru>
2024-04-21 18:25:14 +0300

    update vite.config

--- vite.config.js
+++ vite.config.js
@@ -10,9 +10,9 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/topics.json': 'http://localhost:3000',
-      '/blogs.json': 'http://localhost:3000',
-      '/lastMessages.json': 'http://localhost:3000',
+      '/topics.json': 'https://dpg-co6qgumv3ddc73c79nr0-a.oregon-postgres.render.com:3000',
+      '/blogs.json': 'https://dpg-co6qgumv3ddc73c79nr0-a.oregon-postgres.render.com:3000',
+      '/lastMessages.json': 'https://dpg-co6qgumv3ddc73c79nr0-a.oregon-postgres.render.com:3000',
     },
   },
 });
