453f7e3a9e91ac99a8b267d6cdfc02ac7ea07fc9
Grom Gromov <khalid.096@bk.ru>
2024-04-27 19:52:15 +0300

    delete messages

--- app.js
+++ app.js
@@ -54,6 +54,19 @@ app.get('/blogs.json', (req, res) => {
   });
 });
 
+app.delete('/lastMessages/:id.json', (req, res) => {
+  const messageId = req.params.id;
+
+  pool.query('DELETE FROM lastMessages WHERE id = $1', [messageId], (err, result) => {
+    if (err) {
+      console.error('Ошибка выполнения запроса', err);
+      res.status(500).json({ error: 'Произошла ошибка при удалении сообщения' });
+    } else {
+      res.json({ message: 'Сообщение успешно удалено' });
+    }
+  });
+});
+
 app.listen(port, () => {
   console.log(`Сервер запущен на порту ${port}`);
 });
