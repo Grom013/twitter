6de3650153e2e1eeb213a420d3348672cf8c6cf5
Grom Gromov <khalid.096@bk.ru>
2024-05-08 12:50:19 +0300

    add create-user

--- tests/check-mail.js
+++ tests/check-mail.js
@@ -7,31 +7,37 @@ describe('Функция проверки email', function () {
     const result = mail('khalid.096@bk.ru');
     assert.equal(expectedResult, result);
   });
+
   it('если начинается с запрещенных символов', function () {
     const expectedResult = false;
     const result = mail('.khalid.096@bk.ru');
     assert.equal(expectedResult, result);
   });
+
   it('если первая часть меньше или равна 3 символам', function () {
     const expectedResult = false;
     const result = mail('kha@bk.ru');
     assert.equal(expectedResult, result);
   });
+
   it('если нет точки во второй части', function () {
     const expectedResult = false;
     const result = mail('khalid.096@bkru');
     assert.equal(expectedResult, result);
   });
+
   it('если во второй части после точки меньше 2 символов', function () {
     const expectedResult = false;
     const result = mail('khalid.096@bk.r');
     assert.equal(expectedResult, result);
   });
+
   it('если во второй части до точки меньше 2 символов', function () {
     const expectedResult = false;
     const result = mail('khalid.096@b.ru');
     assert.equal(expectedResult, result);
   });
+
   it('если есть запрещенные символы', function () {
     const expectedResult = false;
     const result = mail('khal[id.096@bk.ru');
--- tests/time.test.js
+++ tests/time.test.js
@@ -7,51 +7,61 @@ describe('Функция преобразования числа во время
     const result = timeFn(366 * 24 * 60);
     assert.equal(expectedResult, result);
   });
+
   it('выходим за рамки рамки 60 минут', function () {
     const expectedResult = '1 час назад';
     const result = timeFn(63);
     assert.equal(expectedResult, result);
   });
+
   it('выходим за рамки рамки 24 часов', function () {
     const expectedResult = '1 день назад';
     const result = timeFn(25 * 60);
     assert.equal(expectedResult, result);
   });
+
   it('проверяем спряжение', function () {
     const expectedResult = '2 минуты назад';
     const result = timeFn(2);
     assert.equal(expectedResult, result);
   });
+
   it('проверяем спряжение', function () {
     const expectedResult = '5 минут назад';
     const result = timeFn(5);
     assert.equal(expectedResult, result);
   });
+
   it('проверяем спряжение', function () {
     const expectedResult = '21 минуту назад';
     const result = timeFn(21);
     assert.equal(expectedResult, result);
   });
+
   it('проверяем спряжение', function () {
     const expectedResult = '11 минут назад';
     const result = timeFn(11);
     assert.equal(expectedResult, result);
   });
+
   it('проверяем спряжение', function () {
     const expectedResult = '2 часа назад';
     const result = timeFn(2 * 60);
     assert.equal(expectedResult, result);
   });
+
   it('проверяем спряжение', function () {
     const expectedResult = '6 часов назад';
     const result = timeFn(6 * 60);
     assert.equal(expectedResult, result);
   });
+
   it('проверяем спряжение', function () {
     const expectedResult = '2 дня назад';
     const result = timeFn(2 * 24 * 60);
     assert.equal(expectedResult, result);
   });
+
   it('проверяем спряжение', function () {
     const expectedResult = '2 дня назад';
     const result = timeFn(2 * 24 * 60);
