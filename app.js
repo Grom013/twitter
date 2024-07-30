a2e63738afdd9881d0cd22c1d626bffe01e7e66b
Grom Gromov <khalid.096@bk.ru>
2024-07-30 20:12:38 +0300

    fix errors4.4

--- app.js
+++ app.js
@@ -29,143 +29,143 @@ const pool = new Pool({
 });
 app.use(express.json());
 
-app.get('/topics.json', (req, res) => {
-  pool.query('SELECT * FROM topics', (err, result) => {
-    if (err) {
-      console.error('Ошибка выполнения запроса', err);
-      res.status(500).json({ error: 'Произошлa ошибка при получении данных' });
-    } else {
-      const topicsData = result.rows;
-      res.json(topicsData);
-    }
-  });
-});
+// app.get('/topics.json', (req, res) => {
+//   pool.query('SELECT * FROM topics', (err, result) => {
+//     if (err) {
+//       console.error('Ошибка выполнения запроса', err);
+//       res.status(500).json({ error: 'Произошлa ошибка при получении данных' });
+//     } else {
+//       const topicsData = result.rows;
+//       res.json(topicsData);
+//     }
+//   });
+// });
 
-app.get('/lastMessages.json', (req, res) => {
-  pool.query('SELECT * FROM lastMessages', (err, result) => {
-    if (err) {
-      console.error('Ошибка выполнения запроса', err);
-    } else {
-      const lastMessagesData = result.rows;
-      res.json(lastMessagesData);
-    }
-  });
-});
+// app.get('/lastMessages.json', (req, res) => {
+//   pool.query('SELECT * FROM lastMessages', (err, result) => {
+//     if (err) {
+//       console.error('Ошибка выполнения запроса', err);
+//     } else {
+//       const lastMessagesData = result.rows;
+//       res.json(lastMessagesData);
+//     }
+//   });
+// });
 
-app.get('/blogs.json', (req, res) => {
-  pool.query('SELECT * FROM blogs', (err, result) => {
-    if (err) {
-      console.error('Ошибка выполнения запроса', err);
-    } else {
-      const blogsData = result.rows;
-      res.json(blogsData);
-    }
-  });
-});
+// app.get('/blogs.json', (req, res) => {
+//   pool.query('SELECT * FROM blogs', (err, result) => {
+//     if (err) {
+//       console.error('Ошибка выполнения запроса', err);
+//     } else {
+//       const blogsData = result.rows;
+//       res.json(blogsData);
+//     }
+//   });
+// });
 
-app.delete('/lastMessages/:id.json', (req, res) => {
-  const messageId = req.params.id;
+// app.delete('/lastMessages/:id.json', (req, res) => {
+//   const messageId = req.params.id;
 
-  pool.query('DELETE FROM lastMessages WHERE id = $1', [messageId], (err, result) => {
-    if (err) {
-      console.error('Ошибка выполнения запроса', err);
-      res.status(500).json({ error: 'Произошла ошибка при удалении сообщения' });
-    } else {
-      res.json({ message: 'Сообщение успешно удалено' });
-    }
-  });
-});
+//   pool.query('DELETE FROM lastMessages WHERE id = $1', [messageId], (err, result) => {
+//     if (err) {
+//       console.error('Ошибка выполнения запроса', err);
+//       res.status(500).json({ error: 'Произошла ошибка при удалении сообщения' });
+//     } else {
+//       res.json({ message: 'Сообщение успешно удалено' });
+//     }
+//   });
+// });
 
-app.post('/lastMessages.json', (req, res) => {
-  const { message } = req.body;
+// app.post('/lastMessages.json', (req, res) => {
+//   const { message } = req.body;
 
-  pool.query('INSERT INTO lastMessages (message) VALUES ($1)', [message], (err, result) => {
-    if (err) {
-      console.error('Ошибка выполнения запроса', err);
-      res.status(500).json({ error: 'Произошла ошибка при создании сообщения' });
-    } else {
-      res.status(201).json({ message: 'Сообщение успешно создано' });
-    }
-  });
-});
+//   pool.query('INSERT INTO lastMessages (message) VALUES ($1)', [message], (err, result) => {
+//     if (err) {
+//       console.error('Ошибка выполнения запроса', err);
+//       res.status(500).json({ error: 'Произошла ошибка при создании сообщения' });
+//     } else {
+//       res.status(201).json({ message: 'Сообщение успешно создано' });
+//     }
+//   });
+// });
 
-app.delete('/lastMessages/:id.json', (req, res) => {
-  const messageId = req.params.id;
+// app.delete('/lastMessages/:id.json', (req, res) => {
+//   const messageId = req.params.id;
 
-  pool.query('DELETE FROM lastMessages WHERE id = $1', [messageId], (err, result) => {
-    if (err) {
-      console.error('Ошибка выполнения запроса', err);
-      res.status(500).json({ error: 'Произошла ошибка при удалении сообщения' });
-    } else {
-      res.json({ message: 'Сообщение успешно удалено' });
-    }
-  });
-});
+//   pool.query('DELETE FROM lastMessages WHERE id = $1', [messageId], (err, result) => {
+//     if (err) {
+//       console.error('Ошибка выполнения запроса', err);
+//       res.status(500).json({ error: 'Произошла ошибка при удалении сообщения' });
+//     } else {
+//       res.json({ message: 'Сообщение успешно удалено' });
+//     }
+//   });
+// });
 
-app.post('/lastMessages/:id.json', (req, res) => {
-  const messageId = req.params.id;
-  const { message } = req.body;
+// app.post('/lastMessages/:id.json', (req, res) => {
+//   const messageId = req.params.id;
+//   const { message } = req.body;
 
-  pool.query('UPDATE lastMessages SET message = $1 WHERE id = $2', [message, messageId], (err, result) => {
-    if (err) {
-      console.error('Ошибка выполнения запроса', err);
-      res.status(500).json({ error: 'Произошла ошибка при редактировании сообщения' });
-    } else {
-      res.json({ message: 'Сообщение успешно отредактировано' });
-    }
-  });
-});
+//   pool.query('UPDATE lastMessages SET message = $1 WHERE id = $2', [message, messageId], (err, result) => {
+//     if (err) {
+//       console.error('Ошибка выполнения запроса', err);
+//       res.status(500).json({ error: 'Произошла ошибка при редактировании сообщения' });
+//     } else {
+//       res.json({ message: 'Сообщение успешно отредактировано' });
+//     }
+//   });
+// });
 
-app.post('/createUser', async (req, res) => {
-  const { email, password } = req.body;
+// app.post('/createUser', async (req, res) => {
+//   const { email, password } = req.body;
 
-  try {
-    const salt = await bcrypt.genSalt(10);
-    const hashedPassword = await bcrypt.hash(password, salt);
+//   try {
+//     const salt = await bcrypt.genSalt(10);
+//     const hashedPassword = await bcrypt.hash(password, salt);
 
-    pool.query('INSERT INTO users (email, password) VALUES ($1, $2)', [email, hashedPassword], (err, result) => {
-      if (err) {
-        console.error('Ошибка выполнения запроса', err);
-        res.status(500).json({ error: 'Произошла ошибка при создании пользователя' });
-      } else {
-        res.status(201).json({ message: 'Пользователь успешно создан' });
-      }
-    });
-  } catch (error) {
-    console.error('Ошибка при хешировании пароля:', error);
-    res.status(500).json({ error: 'Произошла ошибка при создании пользователя' });
-  }
-});
+//     pool.query('INSERT INTO users (email, password) VALUES ($1, $2)', [email, hashedPassword], (err, result) => {
+//       if (err) {
+//         console.error('Ошибка выполнения запроса', err);
+//         res.status(500).json({ error: 'Произошла ошибка при создании пользователя' });
+//       } else {
+//         res.status(201).json({ message: 'Пользователь успешно создан' });
+//       }
+//     });
+//   } catch (error) {
+//     console.error('Ошибка при хешировании пароля:', error);
+//     res.status(500).json({ error: 'Произошла ошибка при создании пользователя' });
+//   }
+// });
 
-app.post('/login', async (req, res) => {
-  const { email, password } = req.body;
+// app.post('/login', async (req, res) => {
+//   const { email, password } = req.body;
 
-  try {
-    const result = await pool.query('SELECT * FROM users WHERE email = $1', [email]);
+//   try {
+//     const result = await pool.query('SELECT * FROM users WHERE email = $1', [email]);
 
-    if (result.rows.length > 0) {
-      const user = result.rows[0];
-      const passwordMatch = await bcrypt.compare(password, user.password);
+//     if (result.rows.length > 0) {
+//       const user = result.rows[0];
+//       const passwordMatch = await bcrypt.compare(password, user.password);
 
-      if (passwordMatch) {
-        const token = crypto.randomUUID();
-        await pool.query('INSERT INTO sessions (email, token) VALUES ($1, $2)', [email, token]);
+//       if (passwordMatch) {
+//         const token = crypto.randomUUID();
+//         await pool.query('INSERT INTO sessions (email, token) VALUES ($1, $2)', [email, token]);
 
-        res.cookie('token', token, { httpOnly: true });
-        res.cookie('email', email, { httpOnly: true });
+//         res.cookie('token', token, { httpOnly: true });
+//         res.cookie('email', email, { httpOnly: true });
 
-        res.status(200).json({ message: 'Успешная аутентификация', email, token });
-      } else {
-        res.status(401).json({ error: 'Неверный пароль' });
-      }
-    } else {
-      res.status(404).json({ error: 'Пользователь с таким email не найден' });
-    }
-  } catch (err) {
-    console.error('Ошибка выполнения запроса', err);
-    res.status(500).json({ error: 'Произошла ошибка при проверке пользователя' });
-  }
-});
+//         res.status(200).json({ message: 'Успешная аутентификация', email, token });
+//       } else {
+//         res.status(401).json({ error: 'Неверный пароль' });
+//       }
+//     } else {
+//       res.status(404).json({ error: 'Пользователь с таким email не найден' });
+//     }
+//   } catch (err) {
+//     console.error('Ошибка выполнения запроса', err);
+//     res.status(500).json({ error: 'Произошла ошибка при проверке пользователя' });
+//   }
+// });
 
 // async function isValidToken(token) {
 //   try {
@@ -190,16 +190,14 @@ app.post('/login', async (req, res) => {
 //   }
 // }
 
-app.get('/feed', async (req, res) => {
-  console.log('GET /feed called');
+app.get('/feed', async (req, res) =>
 
   // if (!token || !(await isValidToken(token))) {
   //   res.clearCookie('token');
   //   res.clearCookie('email');
   //   return res.redirect('/');
   // }
-  return res.send('страница FEED');
-});
+  res.send('страница FEED'));
 
 // app.get('/clearCookie', async (req, res) => {
 
